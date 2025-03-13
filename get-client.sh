ba74d65855334c5e166fdb229a6f616e46ac40ff
Eliot Malsert <eliot.malsert@gmail.com>
2025-03-13 12:23:57 +0100

    Add JAR-based client distribution

--- get-client.sh
+++ get-client.sh
@@ -4,13 +4,19 @@
 mkdir -p ~/chat-client
 cd ~/chat-client
 
-# Download the client JAR
+# Download the client files
 echo "Downloading chat client..."
-curl -s https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/chat-client.jar > chat-client.jar
+wget https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/ChatClient.java
+wget https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/ReadThread.java
+wget https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/WriteThread.java
+
+# Compile the client
+echo "Compiling chat client..."
+javac -source 11 -target 11 *.java
 
 # Run the client
 echo "Starting chat client..."
-java -jar chat-client.jar 167.86.109.247 1234
+java ChatClient 167.86.109.247 1234
 
 # Clean up
 cd ..
