527160b3caf480bb3684507251fe13ed56d20d87
Eliot Malsert <eliot.malsert@gmail.com>
2025-03-13 12:27:25 +0100

    Simplify client installation to compile from source

--- get-client.sh
+++ get-client.sh
@@ -4,13 +4,19 @@
 mkdir -p ~/chat-client
 cd ~/chat-client
 
-# Download the client JAR
+# Download the client files
 echo "Downloading chat client..."
-curl -s https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/chat-client.jar > chat-client.jar
+curl -s https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/ChatClient.java > ChatClient.java
+curl -s https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/ReadThread.java > ReadThread.java
+curl -s https://raw.githubusercontent.com/emalsert/ChatMultiThreadJava/main/client/WriteThread.java > WriteThread.java
+
+# Compile the client
+echo "Compiling chat client..."
+javac -source 11 -target 11 *.java
 
 # Run the client
 echo "Starting chat client..."
-java -jar chat-client.jar 167.86.109.247 1234
+java ChatClient 167.86.109.247 1234
 
 # Clean up
 cd ..
